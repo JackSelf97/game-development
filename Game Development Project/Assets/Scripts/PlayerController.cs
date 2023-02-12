2f3c1d6b5a824f7632a858c1b24ad74f51897fdc
JackSelf97 <selfbrothers@hotmail.com>
2023-02-12 14:57:42 +0000

    Player Controller (0.1) & Cinemachine
    - Started to work on player inputs
    - Added Cinemachine

---
+++ Game Development Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    [SerializeField] private float playerSpeed = 4f;
+    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float gravityValue = -9.81f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    private InputManager inputManager;
+    private Transform camTransform;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
+        camTransform = Camera.main.transform;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+
+        move = camTransform.forward.normalized * move.z + camTransform.right.normalized * move.x;
+        move.y = 0f;
+        controller.Move(move.normalized * Time.deltaTime * playerSpeed);
+
+        //if (move != Vector3.zero)
+        //{
+        //    gameObject.transform.forward = move;
+        //}
+
+        // Changes the height position of the player..
+        if (inputManager.PlayerJumped() && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+
+        // player rotation
+
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        
+    }
+}
