3aa1674d572d13f4080f9b2ff097ad8d854c37bf
JackSelf97 <selfbrothers@hotmail.com>
2023-02-13 16:03:52 +0000

    Player Controller (0.2)
    - Added raycasting
    - Moved camera settings and movement into fixedUpdate

--- Game Development Project/Assets/Scripts/PlayerController.cs
+++ Game Development Project/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,8 @@ public class PlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    [SerializeField] private float playerSpeed = 4f;
-    [SerializeField] private float jumpHeight = 1.0f;
+    [SerializeField] private float playerSpeed = 6f;
+    [SerializeField] private float jumpHeight = 1f;
     [SerializeField] private float gravityValue = -9.81f;
     [SerializeField] private float rotationSpeed = 10f;
 
@@ -26,38 +26,63 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        GroundCheck();
+        JumpInput();
+        PlayerInteraction();
+    }
+
+    void FixedUpdate()
+    {
+        ControllerMovement();   
+    }
+
+    public void ControllerMovement()
+    {
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0f, movement.y);
+        move = camTransform.forward.normalized * move.z + camTransform.right.normalized * move.x;
+        move.y = 0f;
+        controller.Move(move.normalized * Time.fixedDeltaTime * playerSpeed);
+
+        playerVelocity.y += gravityValue * Time.fixedDeltaTime;
+        controller.Move(playerVelocity * Time.fixedDeltaTime);
+
+        // player rotation
+        float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.fixedDeltaTime * rotationSpeed);
+    }
+
+    public void GroundCheck()
+    {
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
+    }
 
-        Vector2 movement = inputManager.GetPlayerMovement();
-        Vector3 move = new Vector3(movement.x, 0f, movement.y);
-
-        move = camTransform.forward.normalized * move.z + camTransform.right.normalized * move.x;
-        move.y = 0f;
-        controller.Move(move.normalized * Time.deltaTime * playerSpeed);
-
-        //if (move != Vector3.zero)
-        //{
-        //    gameObject.transform.forward = move;
-        //}
-
-        // Changes the height position of the player..
+    public void JumpInput()
+    {
+        // changes the height position of the player..
         if (inputManager.PlayerJumped() && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
+    }
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+    public void PlayerInteraction()
+    {
+        RaycastHit hit;
+        const float rayLength = 3;
 
-        // player rotation
-
-            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + camTransform.eulerAngles.y;
-            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
-        
+        Debug.DrawRay(camTransform.position, camTransform.forward.normalized * rayLength, Color.green);
+        if (Physics.Raycast(camTransform.position, camTransform.forward.normalized, out hit, rayLength))
+        {
+            if (hit.collider.CompareTag("Junk"))
+            {
+                Debug.Log("This is junk.");
+            }
+        }
     }
 }
