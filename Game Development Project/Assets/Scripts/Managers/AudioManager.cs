be64e016a11f33b36becd4e5172288e0b3c0cdbd
JackSelf97 <selfbrothers@hotmail.com>
2023-03-17 17:55:27 +0000

    Ragdoll Physics (0.3) & Sound Manager
    - Updated ragdoll physics
    - Implemented a sound manager

---
+++ Game Development Project/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager aMan;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (aMan == null)
+        {
+            aMan = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+            
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound item in sounds)
+        {
+            item.source = gameObject.AddComponent<AudioSource>();
+            item.source.clip = item.clip;
+            item.source.volume = item.volume;
+            item.source.pitch = item.pitch;
+            item.source.loop = item.loop;
+        }
+    }
+
+    private void Start()
+    {
+        //Play("Theme");
+    }
+
+    public void Play(string name)
+    {
+        Sound item = Array.Find(sounds, sound => sound.name == name);
+
+        if (item == null)
+        {
+            Debug.LogError("Sound: " + name + " not found! Have you spelt it correctly?");
+            return;
+        }
+            
+        item.source.Play();
+
+        // trigger sound in code
+        //FindObjectOfType<AudioManager>().Play("PlayerDeath");
+    }
+}
